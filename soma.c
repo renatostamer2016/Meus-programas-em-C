c9382845969183e1a54b28c85e9e07a434537625
renatostamer2016 <rstamermarcovecchio@gmail.com>
2016-09-27 09:09:53 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -6,7 +6,7 @@
 	  int num2;//aqui ele declara o numero 2
 	  
 	     num1 = 10;//aqui ele declara o valor do numero 1
-		 num2 = num1 + 12;//aqui ele declara o valor do numero 2
+	     num2 = num1 + 12;//aqui ele declara o valor do numero 2
 		 
 	  printf ("\nO primeiro numero e %d\n" ,num1);//aqui ele imprime na tela o numero 1
 	  printf ("\nO segundo numero e \"num1 + 12 \" = %d\n" ,num2);//aqui ele imprime na tela o numero 2
