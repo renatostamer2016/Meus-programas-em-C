9fb32b9ba43cc43a22f2cf7c4835788831548e78
renatostamer2016 <rstamermarcovecchio@gmail.com>
2016-09-27 09:09:29 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,16 @@
+#include <stdio.h>//para printf
+#include <stdlib.h>// para system
+  int main()
+  {
+	  int num1;//aqui ele declara o numero 1
+	  int num2;//aqui ele declara o numero 2
+	  
+	     num1 = 10;//aqui ele declara o valor do numero 1
+		 num2 = num1 + 12;//aqui ele declara o valor do numero 2
+		 
+	  printf ("\nO primeiro numero e %d\n" ,num1);//aqui ele imprime na tela o numero 1
+	  printf ("\nO segundo numero e \"num1 + 12 \" = %d\n" ,num2);//aqui ele imprime na tela o numero 2
+	  
+	     system ("PAUSE");
+		   return 0;
+         }
